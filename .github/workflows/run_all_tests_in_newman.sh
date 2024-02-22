e0168b91cc1e907a5192d4313fcc910ee17ce491
Przemyslaw.Kozak.QA <przemyslaw.kozak.qa@gmail.com>
2024-02-22 23:30:14 +0100

    filename fixes

--- .github/workflows/run_all_tests_in_newman.sh
+++ .github/workflows/run_all_tests_in_newman.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Tests execution from all Postman collection files in the directory Postman_tests/ "
 for file in ./Postman_tests/*; do
-    newman run "$file" -e ./Postman_environments/GitHub_Actions.json -r cli,htmlextra --reporter-htmlextra-export "./newman_test_reports/$file-test-report.html"
+    newman run "$file" -e ./Postman_environments/GitHub_Actions.json -r cli,htmlextra --reporter-htmlextra-export "./newman_test_reports/$file-report.html"
 done
 echo "Data-driven tests execution"
-newman run ./Postman_data_driven_tests/Login_negative_tests.postman_collection.json -d ./Postman_data_driven_tests/Login_negative_data.json -e ./Postman_environments/GitHub_Actions.json -r cli,htmlextra --reporter-htmlextra-export ./newman_test_reports/data-driven-test-report.html
+newman run ./Postman_data_driven_tests/Login_negative_tests.json -d ./Postman_data_driven_tests/Login_negative_data.json -e ./Postman_environments/GitHub_Actions.json -r cli,htmlextra --reporter-htmlextra-export ./newman_test_reports/data-driven-test-report.html
