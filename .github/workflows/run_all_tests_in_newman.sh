dc4b925037e189de84f545b644da94686bea83f3
Przemyslaw.Kozak.QA <przemyslaw.kozak.qa@gmail.com>
2024-02-22 22:21:52 +0100

    Update gh-actions settings

--- .github/workflows/run_all_tests_in_newman.sh
+++ .github/workflows/run_all_tests_in_newman.sh
@@ -4,4 +4,4 @@ for file in ./Postman_tests/*; do
     newman run "$file" -e ./Postman_environments/GitHub_Actions.json -r cli,htmlextra
 done
 echo "Data-driven tests execution"
-newman run ./Postman_data_driven_tests/Login_negative_tests.postman_collection.json -d ./Postman_data_driven_tests/Login_negative_data.json -e ./Postman_environments/GitHub_Actions.json -r cli,htmlextra
+newman run ./Postman_data_driven_tests/Login_negative_tests.postman_collection.json -d ./Postman_data_driven_tests/Login_negative_data.json -e ./Postman_environments/GitHub_Actions.json -r cli,htmlextra --reporter-htmlextra-export ./newman_test_reports/test-report.html
